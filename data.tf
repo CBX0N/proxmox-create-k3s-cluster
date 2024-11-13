63afdc52e3ddea733bd07b80f0c142387a69ad1e
charlie boon <charlie.boon@sky.com>
2024-11-13 13:41:09 +0000

    Adding data block for kubeconfig

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-# data "local_file" "kubeconfig" {
-#   depends_on = [ null_resource.get_kubeconfig ]
-#   filename = "${path.module}/k3s.yaml"
-# }
+data "local_file" "kubeconfig" {
+  depends_on = [ null_resource.get_kubeconfig ]
+  filename = "${path.module}/k3s.yaml"
+}
