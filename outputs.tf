48b9ce105dad61d93db7e87286255d4788f8a232
charlie boon <charlie.boon@sky.com>
2024-11-13 16:52:23 +0000

    Adding data block for kubeconfig

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,18 @@ output "kubeconfig_certificate_authority" {
   value = local.kubeconfig.clusters[0].cluster.certificate-authority-data
 }
 
+# output "kubeconfig_client_certificate" {
+#   value = local.kubeconfig.clusters[0].users[0].user.client-certificate-data
+# }
+
+# output "kubeconfig_client_key" {
+#   value = local.kubeconfig.clusters[0].users[0].user.client-key-data
+# }
+
 output "kubeconfig_client_certificate" {
-  value = local.kubeconfig.clusters[0].users[0].user.client-certificate-data
+  value = local.kubeconfig.clusters[0]
 }
 
 output "kubeconfig_client_key" {
-  value = local.kubeconfig.clusters[0].users[0].user.client-key-data
+  value = local.kubeconfig.clusters[0]
 }
